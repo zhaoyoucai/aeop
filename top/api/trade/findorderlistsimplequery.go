924715a6003ed074f53df59032647e54fca23257
zhaoyoucai <zhaoyc128@163.com>
2019-09-05 22:43:45 +0800

    订单简化列表

--- top/api/trade/findorderlistsimplequery.go
+++ top/api/trade/findorderlistsimplequery.go
@@ -7,6 +7,35 @@ type FindOrderListSimpleQueryAPI struct {
 	Api top.API
 }
 
+type FindOrderListSimpleQueryResponse struct {
+	Result struct {
+		OrderList []struct {
+			GmtModified string `json:"gmt_modified"`
+			OrderID int64 `json:"order_id"`
+			Memo string `json:"memo,omitempty"`
+			GmtCreate string `json:"gmt_create"`
+			BizType string `json:"biz_type"`
+			ProductList []struct {
+				GoodsPrepareTime int `json:"goods_prepare_time"`
+				ProductImgURL string `json:"product_img_url"`
+				ProductID int64 `json:"product_id"`
+				ProductSnapURL string `json:"product_snap_url"`
+				ProductUnitPrice string `json:"product_unit_price"`
+				MoneyBack3X bool `json:"money_back3x"`
+				ProductUnit string `json:"product_unit"`
+				ProductCount int `json:"product_count"`
+				ChildID int64 `json:"child_id"`
+				SonOrderStatus string `json:"son_order_status"`
+				ProductName string `json:"product_name"`
+				ProductUnitPriceCur string `json:"product_unit_price_cur"`
+			} `json:"product_list"`
+			OrderStatus string `json:"order_status"`
+		} `json:"order_list"`
+		TotalItem int `json:"total_item"`
+	} `json:"result"`
+	RequestID string `json:"request_id"`
+}
+
 func  (a *FindOrderListSimpleQueryAPI)Init(session string)  {
 
 	ae := top.Aliexpress{}
